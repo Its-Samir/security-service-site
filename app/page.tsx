d934ad08766fdcb2275f4e044fb8c7b3c7f2bd09
samir <shubhod2k18@gmail.com>
2024-04-11 21:35:33 +0530

    services section added

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/components/Header";
 import Navbar from "@/components/Navbar";
+import Services from "@/components/Services";
 
 export default function Home() {
 	return (
-		<main className="flex min-h-screen flex-col px-4 md:px-0 w-full md:w-4/5 mx-auto">
+		<main className="flex space-y-3 min-h-screen flex-col px-4 md:px-0 w-full md:w-4/5 mx-auto">
 			<Navbar />
 			<Header />
+			<Services />
 		</main>
 	);
 }
---
+++ components/Services.tsx
@@ -0,0 +1,52 @@
+import { ArrowRightIcon, BellElectricIcon, ShieldEllipsis, User2 } from "lucide-react";
+import Button from "./Button";
+
+export default function Services() {
+	return (
+		<section id="services" className="space-y-2">
+			<h2 className="text-2xl text-neutral-800 font-sans font-bold">
+				Our Services
+			</h2>
+			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+				<div className="space-y-2 p-2 border-0 border-b">
+               <ShieldEllipsis color="#2563eb" size={30} />
+					<h3 className="text-lg md:text-xl text-neutral-600 font-bold font-sans">
+						Security Consulting
+					</h3>
+					<p className="text-slate-700 text-sm md:text-base">
+						We provide expert security consulting services tailored to
+						your needs.
+					</p>
+					<Button className="flex gap-1 items-center bg-transparent border-blue-600 text-blue-600 shadow-none">
+						Check Out <ArrowRightIcon size={16} />
+					</Button>
+				</div>
+				<div className="space-y-2 p-2 border-0 border-b">
+               <User2 color="#2563eb" size={30} />
+					<h3 className="text-lg md:text-xl text-neutral-600 font-bold font-sans">
+						Guard Services
+					</h3>
+					<p className="text-slate-700 text-sm md:text-base">
+						Our trained guards ensure the safety and security of your
+						premises.
+					</p>
+					<Button className="flex gap-1 items-center bg-transparent border-blue-600 text-blue-600 shadow-none">
+						Check Out <ArrowRightIcon size={16} />
+					</Button>
+				</div>
+				<div className="space-y-2 p-2 border-0 border-b">
+               <BellElectricIcon color="#2563eb" size={30} />
+					<h3 className="text-lg md:text-xl text-neutral-600 font-bold font-sans">
+						Alarm Systems
+					</h3>
+					<p className="text-slate-700 text-sm md:text-base">
+						State-of-the-art alarm 7ystems to protect your property.
+					</p>
+					<Button className="flex gap-1 items-center bg-transparent border-blue-600 text-blue-600 shadow-none">
+						Check Out <ArrowRightIcon size={16} />
+					</Button>
+				</div>
+			</div>
+		</section>
+	);
+}
